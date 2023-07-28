8d6d52666c9c203e663c18f55cc38774a856a531
Sethgood7 <sethgooodman657@gmail.com>
2023-07-27 21:23:22 -0700

    another commit just in case

---
+++ assets/soccer.js
@@ -0,0 +1,6 @@
+function kickBall(){
+    //create event listener for calling the function on spacebar
+    //when called, ball is kicked up the screen
+    //also when called, the image will convert to the gif to
+    // create a rolling effect
+}
