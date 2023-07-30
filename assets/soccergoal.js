cda3dd055439981d50f92d2fe0fa37c5bc9f743b
Sethgood7 <sethgooodman657@gmail.com>
2023-07-30 11:14:29 -0700

    started making soccer goal moveable

---
+++ assets/soccergoal.js
@@ -0,0 +1,14 @@
+function move(element){
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+    }
+}
